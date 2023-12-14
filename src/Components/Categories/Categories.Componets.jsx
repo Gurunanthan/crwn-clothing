42aab335ba16aee3de1729fd7eb8d026d6699efb
Gurunanthan <gurunanthan.r.j@gmail.com>
2023-12-14 14:02:33 +0530

    The Project is added with the directory and seperate componnet of first page of the website

---
+++ src/Components/Categories/Categories.Componets.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Categories.Styles.css";
+import Directory from "../Directory/Directory.Components";
+const Categories = () => {
+  return (
+    <div className="categories-container">
+      <Directory />
+    </div>
+  );
+};
+
+export default Categories;
